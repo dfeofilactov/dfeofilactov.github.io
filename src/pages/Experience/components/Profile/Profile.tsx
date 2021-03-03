5f321f3ef61ef501d2ea289a4d1f4093be996949
Denis Feofilactov <denis.feofilactov@gmail.com>
2021-03-03 12:37:44 +0200

    Styles files

---
+++ src/pages/Experience/components/Profile/Profile.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Profile: React.FC = () => {
+  return <div className="profile">Profile</div>;
+};
+
+export default Profile;
---
+++ src/pages/Experience/components/Work/Work.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Work: React.FC = () => {
+  return <div className="work">Work</div>;
+};
+
+export default Work;
